95dde870014e8c33f65733c93b54fb8b2e2d1783
Sreekanth <sreekanth.reddy@nightingales.in>
2019-06-19 10:13:52 +0530

    new commit

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const config = {
 var config = {
     user: 'SAEEsa',
     password: 'gyrit@123',
-    server: '13.234.235.89',
+    server: 'localhost',
     //server: 'CORPSSPS01\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
     database: 'SAEEdb',
     stream: true,
