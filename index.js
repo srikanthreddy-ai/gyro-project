7800068b3c7f25fd548d20c3b75813f72d9a244c
Sreekanth <sreekanth.reddy@nightingales.in>
2019-09-11 19:30:36 +0530

    updates

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ app.use(responseTime());
 var config = {
     user: 'SAEEsa',
     password: 'gyrit@123',
-    server: '13.234.235.89',
-    //server: 'CORPSSPS01\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
+    //server: '13.234.235.89',
+    server: 'CORPSSPS01\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
     database: 'SAEEdb',
     stream: true,
     port:1433,
